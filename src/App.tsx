cb43c1b10877f09ebcebcd1aa4b58e9ae3754d04
AlexWasHeree <victoralexandre2303@gmail.com>
2022-10-14 17:25:27 -0300

    home page ex

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,76 @@
-import './styles/global.css'
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import './styles/global.css';
 
 export function App() {
-
   return (
-   <div>My app</div>
-  )
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Heading size="lg" className="mt-4">
+          React Design System
+        </Heading>
+        <Text size="lg" className="text-gray-400 mt-1">
+          Login or create an account
+        </Text>
+      </header>
+
+      <form className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Email adress</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input
+              type="email"
+              id="email"
+              placeholder="Type your email"
+            ></TextInput.Input>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Password</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input
+              type="password"
+              id="password"
+              placeholder="********"
+            ></TextInput.Input>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">
+            Remember me for 30 days
+          </Text>
+        </label>
+
+        <Button type="submit" className="mt-4">
+          Submit
+        </Button>
+
+        <footer className="flex flex-col items-center gap-4 mt-8">
+          <Text asChild size="sm">
+            <a href="" className="text-gray-400 underline hover:text-gray-200">
+              Forgot password?
+            </a>
+          </Text>
+          <Text asChild size="sm">
+            <a href="" className="text-gray-400 underline hover:text-gray-200">
+              Create a new account
+            </a>
+          </Text>
+        </footer>
+      </form>
+    </div>
+  );
 }
